b1b0af412eebf9623142cf0937f0d91c81ca9bda
zhen-pal <zhenya-makarova@yandex.ru>
2026-06-17 22:26:05 +0300

    Исправлен путь для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: './', // Относительный путь - работает с любым именем репозитория
+  base: '/daily-tasks/',
 })
